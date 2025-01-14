9a599e44580873574753385f131d778b920c9f1d
nicosampler <nf.dominguez.87@gmail.com>
2025-01-13 23:09:33 -0300

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
@@ -43,6 +44,7 @@ export default function RootLayout({
             {children}
           </main>
           <Footer />
+          <Analytics />
         </div>
       </body>
     </html>
