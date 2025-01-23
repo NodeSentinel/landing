c5006c282498ebb3a881f145078373cbdc4b0a90
nicosampler <nf.dominguez.87@gmail.com>
2025-01-23 19:07:49 -0300

    wip login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,45 +1,30 @@
 "use client";
 
-import Script from "next/script";
+import { LoginButton, TelegramAuthData } from "@telegram-auth/react";
 import { Card, CardContent } from "@/components/ui/card";
-import { useEffect } from "react";
-
-declare global {
-  interface Window {
-    onTelegramAuth: (user: {
-      first_name: string;
-      last_name: string;
-      id: number;
-      username: string;
-    }) => void;
-  }
-}
 
 export default function LoginPage() {
-  useEffect(() => {
-    window.onTelegramAuth = (user) => {
-      alert(
-        "Logged in as " +
-          user.first_name +
-          " " +
-          user.last_name +
-          " (" +
-          user.id +
-          (user.username ? ", @" + user.username : "") +
-          ")"
-      );
-    };
-  }, []);
+  const handleAuth = (data: TelegramAuthData) => {
+    alert(
+      "Logged in as " +
+        data.first_name +
+        " " +
+        data.last_name +
+        " (" +
+        data.id +
+        (data.username ? ", @" + data.username : "") +
+        ")"
+    );
+  };
 
   return (
     <div className="container flex items-center justify-center min-h-screen py-12">
       <Card className="w-full max-w-md">
         <CardContent className="flex flex-col items-center justify-center space-y-4">
-          <Script
-            src="https://telegram.org/js/telegram-widget.js?22"
-            data-telegram-login="gbc_validators_bot"
-            data-size="large"
-            data-onauth="onTelegramAuth(user)"
+          <LoginButton
+            botUsername="gbc_validators_bot"
+            onAuthCallback={handleAuth}
+            buttonSize="large"
           />
         </CardContent>
       </Card>
